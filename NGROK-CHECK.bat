a6a53e57fdab8125d461ece762bca55fc8ebcf93
Sachindu443 <76847689+sachindu443@users.noreply.github.com>
2021-07-29 23:51:07 -0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,9 +2,9 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-curl -O https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/EVKey64.exe > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/sachindu443/Temp-WindowsRDP/main/DisablePasswordComplexity.ps1?token=ASKJUSJEURHEADYLSUGOKNTBAOQI6 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/sachindu443/Temp-WindowsRDP/main/FastConfigVPS_v5.1.exe?token=ASKJUSNMUI5CAYJK5ZZMJB3BAOQVS > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/sachindu443/Temp-WindowsRDP/main/EVKey64.exe?token=ASKJUSPJSYZ3FWL3EDFCH6TBAOQR2 > out.txt 2>&1
 net user administrator Sachiya123. /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
